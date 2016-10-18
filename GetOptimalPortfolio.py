871d0b85a6dd4dcb5c4c981d92a61809856a896c
Hugo Fallourd <hugo.fallourd@gmail.com>
2016-10-17 22:32:42 -0400

    Integrate Black Litterman with portfolio optimization

--- GetOptimalPortfolio.py
+++ GetOptimalPortfolio.py
@@ -4,39 +4,33 @@ Created on Sat Oct 15 14:00:23 2016
 
 @author: Hugo
 """
-import quandl
 import numpy
 import math
 from numpy.linalg import inv
 import pandas
 from pymongo import MongoClient
 from GetDataSerieFromMongo import GetDataSerieFromMongo
+from BlackLitterman import bl_omega
+from BlackLitterman import altblacklitterman
 
-def GetOptimalPortfolio(benchmark,apiKey,startDate,endDate):
-
-    quandl.ApiConfig.api_key = apiKey
-    data = quandl.get(benchmark,start_date=startDate, end_date=endDate)
-    benchmarkData = data[['Index Value']]
-    benchmarkData = benchmarkData.dropna()
-    
-    init = (benchmarkData.iloc[0].values)[0]
-    benchmarkData[['Index Value']] = benchmarkData[['Index Value']]/init
+def GetOptimalPortfolio(benchmark,apiKey,startDate,endDate,histWindow,rebalanceFrequency,
+                        tau,stockView,stockViewReturn,stockConfidence):
     
     client = MongoClient()
     db = client.Project
     
     stocks =[x['BBGTicker'] for x in list(db.Stocks.find({}))]
+    stocks = [x for x in stocks if x not in ['GOOG','AVGO','BRCM','CHTR','CMCSK','DISCK','FB','GMCR','KHC','LMCA','LVNTA','SNDK','TSLA','TRIP','VRSK','WBA']]
 
     df = pandas.DataFrame([''],index=['1900-01-01'])
     dfHeaders = ['Dummy']
     
     for stock in stocks:
-        if stock not in ['GOOG','AVGO','BRCM','CHTR','CMCSK','DISCK','FB','GMCR','KHC','LMCA','LVNTA','SNDK','TSLA','TRIP','VRSK','WBA']:      
-            dfHeaders.append(stock)
-            s = GetDataSerieFromMongo(stock,startDate,endDate)   
-            s[0,1] = stock+' Close'
-            tempdf = pandas.DataFrame(s[:,[1]],index=s[:,2])
-            df = pandas.concat([df, tempdf], axis=1,join='outer')
+        dfHeaders.append(stock)
+        s = GetDataSerieFromMongo(stock,startDate,endDate)   
+        s[0,1] = stock+' Close'
+        tempdf = pandas.DataFrame(s[:,[1]],index=s[:,2])
+        df = pandas.concat([df, tempdf], axis=1,join='outer')
             
     df.columns = dfHeaders
     df = df.drop('Dummy',1)
@@ -48,41 +42,71 @@ def GetOptimalPortfolio(benchmark,apiKey,startDate,endDate):
     s = numpy.diff(s.astype(float), axis=0)/s[:-1].astype(float)
     s = s[~numpy.isnan(s).any(axis=1)]
 
-    window=120
-    rebalanceFreq=min(s.shape[0],250)
+    window=histWindow
+    rebalanceFreq=min(s.shape[0],rebalanceFrequency)
     rebalanceTotal = math.floor((s.shape[0] - s.shape[0]%rebalanceFreq-window)/rebalanceFreq)
     i=numpy.ones(s.shape[1])
     rf=0.00001
+
+    P = numpy.zeros((len(stockView),len(stocks)))
+    Q = numpy.zeros((len(stockView),1))
+    
+
    
     portValue=[]
-    htot=[]
-    mutot=[]
+    portBLValue=[]
+
     for k in range(rebalanceTotal+1):
-        Q = numpy.cov(s[k*rebalanceFreq:k*rebalanceFreq+window].T)    
-        Qm1 = inv(Q)
-        mu=numpy.mean(s[k*rebalanceFreq:k*rebalanceFreq+window],axis=0)
-        muP=numpy.mean(mu)
-        muE = mu - rf*i
-        A = numpy.transpose(mu).dot(Qm1.dot(mu))
-        B = numpy.transpose(mu).dot(Qm1.dot(i))
-        C = numpy.transpose(i).dot(Qm1.dot(i))
+        V = 252*numpy.cov(s[k*rebalanceFreq:k*rebalanceFreq+window].T)    
+        Vm1 = inv(V)
+        mu = 252*numpy.mean(s[k*rebalanceFreq:k*rebalanceFreq+window],axis=0)
+        muP = numpy.mean(mu)
+        A = numpy.transpose(mu).dot(Vm1.dot(mu))
+        B = numpy.transpose(mu).dot(Vm1.dot(i))
+        C = numpy.transpose(i).dot(Vm1.dot(i))
         D = A*C-B*B
 
-        h = (C * muP - B)/D * Qm1.dot(mu) + (A - B*muP)/D * Qm1.dot(i)
-        #h=1/(numpy.transpose(i).dot(Qm1.dot(muE))) * (Qm1.dot(muE))
+        h = (C * muP - B)/D * Vm1.dot(mu) + (A - B*muP)/D * Vm1.dot(i)  
+        
+        print(mu)
+        for item in stockView:
+            i = stockView.index(item)
+            j = stocks.index(item)    
+            P[i][j] = 1        
+            Q[i] = mu[j]+stockViewReturn[i]
+            print(Q[i])
+            print(mu[j])
+
+        # Risk aversion of the market 
+        delta = 3.07       
+        # Coefficient of uncertainty in the prior estimate of the mean
+        # from footnote (8) on page 11               
+        tauV = tau * V
+        
+        Omega = numpy.zeros((len(stockConfidence),len(stockConfidence)))
+        for c in range(len(stockConfidence)):
+            Omega[c][c] = bl_omega(stockConfidence[c], P[c], tauV)
+            
+        print(Omega)
+        #Omega = numpy.dot(numpy.dot(P,tauV),P.T)
+        er, hBL, lmbda = altblacklitterman(delta, h, V, tau, P, Q, Omega)        
+        htemp = numpy.reshape(hBL,(1,len(stocks)))
+        hBL = htemp[0]
         if (k==rebalanceTotal):
             temp = df[k*rebalanceFreq+window:df.shape[0]-1].pct_change().dropna().dot(h)
+            tempBL = df[k*rebalanceFreq+window:df.shape[0]-1].pct_change().dropna().dot(hBL)
         else:
-            temp = df[k*rebalanceFreq+window:(k+1)*rebalanceFreq+window].pct_change().dropna().dot(h)                
-        portValue.append(temp)  
-        htot.append(h)
-        mutot.append(mu)
+            temp = df[k*rebalanceFreq+window:(k+1)*rebalanceFreq+window].pct_change().dropna().dot(h) 
+            tempBL = df[k*rebalanceFreq+window:(k+1)*rebalanceFreq+window].pct_change().dropna().dot(hBL)
+            
+        portValue.append(temp)
+        portBLValue.append(tempBL)
 
     temp = portValue[0]
     for p in range(len(portValue)-1):
         temp = pandas.concat([pandas.DataFrame(temp),pandas.DataFrame(portValue[p+1])],axis=0)
     
-    portValue = temp
+    portValue = pandas.DataFrame(temp)
     portValue.index = pandas.to_datetime(portValue.index)
     
     portValue[0][0]=1
@@ -91,6 +115,20 @@ def GetOptimalPortfolio(benchmark,apiKey,startDate,endDate):
             portValue[0][r+1]=0
         portValue[0][r+1]=portValue[0][r]*(1+portValue[0][r+1])
     
-    return portValue,benchmarkData,htot,mutot
+    tempBL = portBLValue[0]
+    for p in range(len(portBLValue)-1):
+        tempBL = pandas.concat([pandas.DataFrame(tempBL),pandas.DataFrame(portBLValue[p+1])],axis=0)
+    
+    portBLValue = pandas.DataFrame(tempBL)
+    portBLValue.index = pandas.to_datetime(portBLValue.index)
+    
+    portBLValue[0][0]=1
+    for r in range(portBLValue.shape[0]-1):
+        if (abs(portBLValue[0][r+1])>1):
+            portBLValue[0][r+1]=0
+        portBLValue[0][r+1]=portBLValue[0][r]*(1+portBLValue[0][r+1])
+        
+        
+    return portValue,portBLValue, h , hBL
     
     
--- Project.py
+++ Project.py
@@ -11,6 +11,7 @@ from CalculateIndicators import CalculateIndicators
 from GetOptimalPortfolio import GetOptimalPortfolio
 import matplotlib.pyplot as plt
 import pandas
+from GetBenchmarkPortfolio import GetBenchmarkPortfolio
 
 today = datetime.date.today()
 today = '2016-09-29'
@@ -21,19 +22,39 @@ benchmark='NASDAQOMX/NDX'
 #CalculateIndicators()
 
 startDate='2010-02-05'
-endDate='2013-10-14'
-portValue,benchmarkValue,h,mu = GetOptimalPortfolio(benchmark,quandlKey,startDate,endDate)
+endDate='2013-10-30'
+histWindow=250
+rebalanceFrequency=2500
+
+stockView = ['YHOO','CSCO','AAPL','GOOGL']
+stockViewReturn = [0.000015,-0.000002,-0.00006,0.00001]
+stockConfidence = [0.1,0.1,0.1,0.1]
+tau = 0.025 
+
+benchmarkValue = GetBenchmarkPortfolio(benchmark,quandlKey,startDate,endDate)
+portValue,portBLValue,h,hBL = GetOptimalPortfolio(benchmark,quandlKey,startDate,endDate,histWindow,rebalanceFrequency,
+                                                  tau,stockView,stockViewReturn,stockConfidence)
 
 
-dfConcatenate = pandas.concat([portValue,benchmarkValue], axis=1, join='inner')
-dfClean=dfConcatenate[[0,'Index Value']]
+portValue.columns = ['OptimalPort']
+portBLValue.columns = ['BlackLittermanPort'] 
+
+dfConcatenate = pandas.concat([portValue,portBLValue,benchmarkValue], axis=1, join='inner')
+init=dfConcatenate['Index Value'][0]
+dfConcatenate['Index Value']=dfConcatenate['Index Value']/init
+dfClean=dfConcatenate[['OptimalPort','BlackLittermanPort','Index Value']]
 
 #Plot benchmark and optimal portfolio
 plt.figure(figsize=(20,7))
 #plt.xticks( dfConcatenate[0], dfConcatenate.index.values )
-port, = plt.plot(dfClean[0],'r',label='Optimal Portfolio')
+port, = plt.plot(dfClean['OptimalPort'],'r',label='Optimal Portfolio')
+portBL, = plt.plot(dfClean['BlackLittermanPort'],'g',label='Black-Litterman Portfolio')
 ben, = plt.plot(dfClean['Index Value'],'b',label='Benchmark')
 
-plt.legend(handles=[port,ben])
+plt.legend(handles=[port,portBL,ben])
 
 plt.show()
+
+
+
+
